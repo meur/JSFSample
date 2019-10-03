b44c0988eda0e73fbd6d4fc6de63d09991be2441
meur <bordanimre@protonmail.com>
2019-10-03 23:14:21 +0200

    bean updated as being SessionScoped

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,22 @@
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @Named
-public class HelloWorld  {
+@SessionScoped
+public class HelloWorld implements Serializable {
+
+	private static final long serialVersionUID = -3662073815964820807L;
 
 	public HelloWorld() {
 		System.out.println("HelloWorld started!");
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("HelloWorld Constructed!");
 	}
 
 	public String getMessage() {
